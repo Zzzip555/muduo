1580444a940a8667af912154f97eb4815ea9aed9
Zzzip555 <62755620+Zzzip555@users.noreply.github.com>
2020-07-27 15:00:10 +0800

    Update Timestamp.cc

--- muduo/base/Timestamp.cc
+++ muduo/base/Timestamp.cc
@@ -32,7 +32,7 @@ string Timestamp::toFormattedString(bool showMicroseconds) const
 {
   char buf[64] = {0};
   time_t seconds = static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
-  struct tm tm_time;
+  struct tm tm_time;//注意区别struct timeval
   gmtime_r(&seconds, &tm_time);
 
   if (showMicroseconds)
